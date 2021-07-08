580c8caacd78075ca9c605eb7efd37cd2f800a1e
Rishi Sharma <technology.rishi@gmail.com>
2021-07-08 06:05:22 +0530

    added two exceptions on same line

--- exception_basic.py
+++ exception_basic.py
@@ -1,4 +1,5 @@
 try:
-    1 / 0
-except ZeroDivisionError:
-    print('I got a ZeroDivisionError')
+    # 1 / 0
+    print(b)
+except (ZeroDivisionError, NameError):
+    print('I got a ZeroDivisionError or Name Error')
